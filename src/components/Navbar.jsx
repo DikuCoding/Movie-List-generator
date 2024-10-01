7c6f669c5392daf46380ba6354b331c7d47fdffc
Dikesh Khagi <dikeshkhagi1@gmail.com>
2024-10-01 19:16:37 +0545

    add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+    <ul className='nav-links'>
+        <li>
+            <a href="#home">Home</a>
+        </li>
+        <li>
+            <a href="#movies">Movies</a>
+        </li>
+        <li>  
+            <a href="#more">More</a>
+        </li>
+    </ul>
+    </div>
+  )
+}
+
+export default Navbar
