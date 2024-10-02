aba26c7bd756e48087bbf2289bd85cfde75225cf
Dikesh Khagi <dikeshkhagi1@gmail.com>
2024-10-02 16:58:05 +0545

    add rating

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMoviesDetails, selectMovieDetails } from '../redux/movieDetailSlice';
@@ -6,7 +6,7 @@ import { fetchMoviesDetails, selectMovieDetails } from '../redux/movieDetailSlic
 const MovieDetail = () => {
     const {movieId} = useParams()
     const [movie, setMovie] = useState({})
-    const [review, setReview] = useState({})
+    const [review, setReview] = useState([])
 
     const dispatch = useDispatch();
     const moviesDetail = useSelector(selectMovieDetails)
@@ -24,7 +24,10 @@ const MovieDetail = () => {
             }
         }
         fetchdata()
+    },[movieId])
 
+    
+    useEffect(()=>{
         const fetchReviews = async()=>{
             // await fetch(`https://api.themoviedb.org/3/movie/${movieId}/reviews?language=en-US&page=1&api_key=${process.env.REACT_APP_API_KEY}`)
             // .then(response => response.json())
@@ -33,16 +36,41 @@ const MovieDetail = () => {
             try {
                 const reviews = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/reviews?language=en-US&page=1&api_key=${process.env.REACT_APP_API_KEY}`)
                 const reviewData = await reviews.json()
-                setReview(reviewData)
+                // setReview(reviewData)
+                dispatch(fetchMoviesDetails(reviewData.results))
+                setReview(reviewData.results)
                 
             } catch (error) {
                 console.log(error)
             }
-            dispatch(fetchMoviesDetails(review.results))
         }
         fetchReviews()
-        
     },[movieId])
+
+    
+    const postReview = useCallback(async()=>{
+        try{
+            const value = document.getElementById("value").value
+            const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/rating`,
+                 {
+                    method: 'POST',
+                    headers: {
+                      accept: 'application/json',
+                      'Content-Type': 'application/json;charset=utf-8',
+                      Authorization: `Bearer ${process.env.REACT_APP_TOKEN}` 
+                    },
+                    body: JSON.stringify({value: value})
+                  }
+            )
+            const reviewData = await response.json()
+            }
+            catch(error){
+                console.log(error)
+            }
+        
+    },[])
+       
+    
   return (
     <div>
     <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} className="" alt="" />
@@ -51,11 +79,32 @@ const MovieDetail = () => {
             <p className="card-text smallText">
               {movie.release_date}
             </p>
-            <h2>Overview/Review</h2>
-            <p>{movie.overview}</p>
             <h2 className=''>Reviews</h2>
-            <p>{review.content}</p>
+            {review.map((card, index)=>(
+                <div>
+                    <p>{card.author}</p>
+                    <p>{card.content}</p>
+                </div>
+            ))}
+
+            <h2>Add a comment:</h2>
+                <label htmlFor="">Username :</label>
+                <input type="text" />
+                <textarea name="" placeholder='Enter your comments' id="">
+                </textarea>
+                <button>SUBMIT</button>
           </div>
+          <h2>Ratings:</h2>
+          {/* form to give rating */}
+            <select name="" id="value">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <button onClick={()=>postReview()}>SUBMIT</button>
+          
     </div>
   )
 }
