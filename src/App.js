bc2488febe69e17971987a0491391a1f4db128d1
DikuCoding <dikeshkhagi1@gmail.com>
2025-01-13 12:25:07 +0545

    remove unnecessary lines cof codes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
 // import './App.css';
 import Navbar from './components/Navbar';
-import Movies from './components/Movies';
-import MovieList from './components/MovieList';
 import { Outlet } from 'react-router-dom';
 
 function App() {
--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, {useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovieDetails, selectMovieDetails } from "../redux/movieSlice";
@@ -24,7 +24,7 @@ const MovieDetail = () => {
       }
     };
     fetchMovieData();
-  }, [movieId]);
+  }, [movieId, dispatch]);
 
   useEffect(() => {
     const fetchReviews = async () => {
@@ -39,7 +39,7 @@ const MovieDetail = () => {
       }
     };
     fetchReviews();
-  }, [movieId]);
+  }, [movieId, dispatch]);
 
   const truncateText = (text, limit) => {
     if (text.length <= limit) return text;
