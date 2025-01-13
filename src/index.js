aa708df322391b37aea8bc2236b7dbb873e1bf01
DikuCoding <dikeshkhagi1@gmail.com>
2025-01-13 12:05:14 +0545

    remove homepage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import MovieList from './components/MovieList';
 import MovieDetail from './components/MovieDetail';
 
